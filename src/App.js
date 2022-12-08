69a614fbeeeb5375040cf4ca8f229be1e39bba65
Abhi-atg-1021 <abhisinghal112@gmail.com>
2022-12-08 19:30:26 +0530

    initial files of react commited

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+    </div>
+  );
+}
+
+export default App;
