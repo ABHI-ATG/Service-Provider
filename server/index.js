fdfa739d7e87e3c6fce00238f65c04b4da7028b7
Arpit-puri <arpit.puri6265@gmail.com>
2023-05-27 12:25:39 +0530

    No idea

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express= require('express');
 const app = express();
 const morgan = require('morgan');
-const port = 8000;
+const port = 8000||process.env.PORT;
 const router = require('./router/route');
 
 //const router= require('router');
