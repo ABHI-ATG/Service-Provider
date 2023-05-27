42b0c9b08ba1ea94110b8ca4cca542be6c24136b
Arpit-puri <arpit.puri6265@gmail.com>
2023-05-27 12:32:17 +0530

    No idea

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const  mongoose= require('mongoose');
 
-const DB= 'mongodb+srv://Servicely:Project123@cluster0.jckugsx.mongodb.net/?retryWrites=true&w=majority';
+const DB= process.env.URL;
 
 mongoose.connect(DB).then(()=>{
   console.log(`DB connected`);
