b17e6516e77495a49a51352a3fd847ae80b7bb5a
Arpit <arpit.puri6265@gmail.com>
2023-07-20 16:31:07 +0530

    adding

--- client/src/DashBoardProvider/Dashboard.js
+++ client/src/DashBoardProvider/Dashboard.js
@@ -1,39 +1,36 @@
-import React, { useContext } from 'react'
-import { userContext } from '../App';
-
+import React, { useContext } from "react";
+import { userContext } from "../App";
+import style from '../Css/dashboard.css'
 const Dashboard = () => {
-  const {state:{provider,message}}=useContext(userContext);
+  const {
+    state: { provider, message },
+  } = useContext(userContext);
   console.log(message);
   console.log(provider);
   return (
-    <div>
-    <div>
-      {provider.fname}
-      <br/>
-      {provider.lname}
-      <br/>
-      {provider.mobile}
-      <br/>
-      {provider.email}
-      <br/>
-      {provider.pincode}
-      <br/>
-      {provider.state}
-      <br/>
-      {provider.city}
+    <div className="Complete">
+      <div className="MainWrapper">
+        <div>{provider.fname}  fnam lname {provider.lname}</div>
+        <div>{provider.mobile} mobile</div>
+        <div>{provider.email} email</div>
+        <div>{provider.pincode} pincode</div>
+        <div>{provider.state} state</div>
+        <div>{provider.city} city</div>
+      </div>
+      <div>
+        <h1>fname lname</h1>
+        <p>Hi! message h  ere</p>
+        {message.map((item) => {
+          return (
+            <div>
+              <h1> fname lname {item.user.fname + " " + item.user.lname}</h1>
+              <p> Hi! message here +{item.latest}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-    <div>
-      {message.map((item)=>{
-        return (
-        <div>
-          <h1>{item.user.fname+" "+item.user.lname}</h1>
-          <p>{item.latest}</p>
-        </div>
-        )
-      })}
-    </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
