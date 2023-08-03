e8d961efa6d7262d6f696ac6ff682b94494bfa52
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-08-03 21:11:53 +0530

    changes

--- client/src/ENDPOINT.js
+++ client/src/ENDPOINT.js
@@ -1,3 +1,3 @@
-// const ENDPOINT = "https://servicely-backend.onrender.com/";
-const ENDPOINT = "http://localhost:8000/";
+const ENDPOINT = "https://servicely-backend.onrender.com/";
+// const ENDPOINT = "http://localhost:8000/";
 export default ENDPOINT;
